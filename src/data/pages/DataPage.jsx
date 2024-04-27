6c9653f4c03684c752f60a07183a26bd1f473b90
Ramiro <63085235+Ciberramir52@users.noreply.github.com>
2024-04-27 18:12:56 -0400

    Start Store

--- src/data/pages/DataPage.jsx
+++ src/data/pages/DataPage.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { NavBar, Footer } from '../../main/components';
 import { TableData } from '../components';
 
 function DataPage() {
+
+    useEffect(() => {
+        new WebSocket("ws://192.168.1.91:3000");
+    }, []);
+
     return ( 
         <div id="page-container">
             <NavBar />
---
+++ src/helpers/getEnvVariables.js
@@ -0,0 +1,5 @@
+export const getEnvVariables = () => {
+    return {
+        VITE_WEBSOCKET_SERVER: 'ws://192.168.1.91:3000',
+    }
+}
