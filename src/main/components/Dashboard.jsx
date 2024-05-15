9d96616dcd6ea4f9a807951ae34e5d05c2f439fc
Ciberramir52 <63085235+Ciberramir52@users.noreply.github.com>
2024-05-15 17:41:47 -0600

    Make websocket funcionality

--- src/main/components/Dashboard.jsx
+++ src/main/components/Dashboard.jsx
@@ -5,25 +5,42 @@ import { useAppDispatch, useDataStore } from "../../hooks";
 export function Dashboard() {
     const { vaccumData, updateVaccumData, socket } = useDataStore();
 
-    const { productCode, testTime, status, minPressure } = vaccumData;
+    const { productCode: product_code, 
+        testTime: test_time,
+        status: status_s,
+        minPressure: min_Pressure } = vaccumData;
 
     const dispatch = useAppDispatch();
 
     socket.addEventListener("message", (event) => {
-        console.log("Message from server ", event.data);
+        const socketData = event.data;
+        const socketDataSplit = socketData.split(":");
+        const [ destination, source, command, info ] = socketDataSplit;
+        if ( destination === "control" ) {
+            const infoSplit = info.split(",");
+            const [ productCode, testTime, status, minPressure ] = infoSplit;
+            const objectData = { productCode, testTime, status, minPressure };
+            console.log(objectData);
+            dispatch( updateVaccumData( objectData ) );
+        }
+        console.log("Message from server ", socketData);
     });
 
-    console.log(vaccumData);
+    const startProcess = () => {
+        socket.send("vaccum:control:start");
+    }
+
+    // console.log(vaccumData);
 
     return ( 
         <main id="dashboard-container">
             <div id="cards-container">
-                <Card title='Product Code' data={ productCode } unit='' />
-                <Card title='Test Time' data={ testTime } unit='s' />
-                <Card title='Status' data={ status } unit='' />
-                <Card title='Min. Pressure' data={ minPressure } unit='kPa' />
+                <Card title='Product Code' data={ product_code } unit='' />
+                <Card title='Test Time' data={ test_time } unit='s' />
+                <Card title='Status' data={ status_s } unit='' />
+                <Card title='Min. Pressure' data={ min_Pressure } unit='kPa' />
             </div>
-            <button type="button">Iniciar Proceso</button>
+            <button onClick={ startProcess } type="button">Iniciar Proceso</button>
         </main>
      );
 }
